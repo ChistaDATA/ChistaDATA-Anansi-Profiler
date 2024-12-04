343332f49d12ca64e664fc0424a74f82fa75ff72
vishal-joshy <83570027+vishal-joshy@users.noreply.github.com>
2024-12-04 14:45:13 +0530

    Update query font

--- pkg/report_templates/clickhouse_resport_templates/accumulated_info_html_template.go
+++ pkg/report_templates/clickhouse_resport_templates/accumulated_info_html_template.go
@@ -10,7 +10,8 @@ const AccumulatedInfoHTMLTemplate = `
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
-    <style>
+	<link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100..700;1,100..700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+	<style>
         *, *::before, *::after {
             box-sizing: border-box;
         }
@@ -93,7 +94,8 @@ const AccumulatedInfoHTMLTemplate = `
 		}
 		.query{
 			background: #F7F7F7;
-			font-family: monospace;
+			font-family: 'Roboto Mono',monospace;
+			font-size: small;
 			padding: 12px;
 		}
 		.stats{
