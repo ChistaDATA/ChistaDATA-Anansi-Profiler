e1d75907d7874e98b12bdb3962c58aa70a19e655
Vishal Joshy <83570027+vishal-joshy@users.noreply.github.com>
2024-12-16 19:24:54 +0530

    Feat postgres html template (#44)
    * Fix query time dist for md, text output
    
    * Update html template for postgres

--- pkg/report_templates/postgres_resport_templates/top_queries_html_template.go
+++ pkg/report_templates/postgres_resport_templates/top_queries_html_template.go
@@ -1,28 +1,33 @@
 package postgres_resport_templates
 
 const TopQueriesHTMLTemplate = `
-<h1>Top Queries</h1>
-<table>
-    <tr>
-        <td>Rank</td>
-        <td>Response time</td>
-		<td>Response time(%)</td>
-        <td>Calls</td>
-        <td>R/Call</td>
-        <td>Query</td>
-        <td></td>
-    </tr>
-    <tr>
-        {{range $record:=.}}
-        <td>{{$record.Pos}}</td>
-        <td>{{$record.TotalDuration}}</td>
-        <td>{{$record.TotalDurationPercentage}}</td>
-        <td>{{$record.Count}}</td>
-        <td>{{$record.ResponseTimePerCall}}</td>
-        <td>{{$record.Query}}</td>
-        {{end}}  
-    </tr>
-</table>
+<section>
+<h3>Top Queries</h3>
+  <table>
+	<thead>
+		<tr>
+		  <td>Rank</td>
+		  <td>Response time</td>
+		  <td>Response time(%)</td>
+		  <td>Calls</td>
+		  <td>R/Call</td>
+		  <td>Query</td>
+		</tr>
+	</thead>
+	<tbody>
+		{{range $record:=.}}
+		<tr>
+		  <td>{{$record.Pos}}</td>
+		  <td>{{$record.TotalDuration}}</td>
+		  <td>{{$record.TotalDurationPercentage}}</td>
+		  <td>{{$record.Count}}</td>
+		  <td>{{$record.ResponseTimePerCall}}</td>
+		  <td>{{$record.Query}}</td>
+		</tr>
+		{{end}}
+	</tbody>
+  </table>
+</section>
 `
 
 const TopQueryHTMLRecord = `<p>| {{.Pos}} | {{.TotalDuration}} {{.TotalDurationPercentage}} |  {{.Count}} | {{.ResponseTimePerCall}} | {{.Query}} |</p>`
